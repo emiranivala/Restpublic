a43fee14a150c1d7287044b9c3096441f5cdbdb2
emiranivala <taitufam@gmail.com>
2025-02-12 09:56:51 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -36,13 +36,13 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("You are Banned. Contact -- @SrijanMajumdar")
+            await message.reply_text("You are Banned. Contact -- @She_who_remain")
             return 1
       except UserNotParticipant:
          await message.reply_photo(photo="https://graph.org/file/a06275c5b4e838e2df0e5-664ae5f4d543dfd30d.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
          return 1
       except Exception:
-         await message.reply_text("Something Went Wrong. Contact us @SrijanMajumdar")
+         await message.reply_text("Something Went Wrong. Contact us @She_who_remain")
          return 1
 
 
